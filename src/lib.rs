ba1012bb462308911235409cd7c88ce91d51810b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2018-02-18 21:04:24 -0500

    Revert "Listing 20-22"
    It's an alternate implementation that doesn't work.
    
    This reverts commit 5492c6b6c97406b28c602cd6d84c4b758d11f1e3.

--- src/lib.rs
+++ src/lib.rs
@@ -65,7 +65,9 @@ struct Worker {
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || {
-            while let Ok(job) = receiver.lock().unwrap().recv() {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
                 println!("Worker {} got a job; executing.", id);
 
                 job.call_box();
